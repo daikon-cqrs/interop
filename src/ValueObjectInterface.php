077214a7ca3ac7fcec4db0000229f4c2114f306e
Hasham Ahmad <mr.hash@gmail.com>
2019-03-29 12:20:48 +0000

    Rename interface parameter

--- src/ValueObjectInterface.php
+++ src/ValueObjectInterface.php
@@ -12,7 +12,7 @@ namespace Daikon\Interop;
 
 interface ValueObjectInterface extends FromNativeInterface, ToNativeInterface
 {
-    public function equals($value): bool;
+    public function equals($comparator): bool;
 
     public function __toString(): string;
 }
