4efb65c3993d172649791a3f5e94227f258efab5
Hasham Ahmad <mr.hash@gmail.com>
2020-06-05 12:12:58 -0500

    Add new exception

---
+++ src/InvalidArgumentException.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the daikon-cqrs/interop project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Daikon\Interop;
+
+use InvalidArgumentException as GlobalInvalidArgumentException;
+
+class InvalidArgumentException extends GlobalInvalidArgumentException implements DaikonException
+{
+}
